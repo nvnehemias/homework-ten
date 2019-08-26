a006061df66e2083f932501a2ecf38d0fb5b0817
Nehemias Vicente <nvnehemias@gmail.com>
2019-08-26 13:45:24 -0700

    More changes to python code

--- scape_Mars.py
+++ scape_Mars.py
@@ -14,12 +14,37 @@ def browser_start():
 
 mars_scrape_info = {}
 
-# NASA Mars News
 
+# NASA Mars News
 def scrape_news():
     # URL of page to be scraped
     url_1 = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
 
+    #Visit website
     browser.visit(url_1)
+
+    #Parse html with Beautiful Soup
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
+
+    #Attain the div class and create a new empty list
+    results = soup.find_all('div',class_ = "content_title")
+    list_title = []
+
+    #Run for loop that outputs the news title 
+    for answer in results:
+        list_title.append(answer.text)
+    news_title = list_title[0]
+    news_title
+
+    #Attain the div class and create a new empty list
+    results_2 = soup.find_all('div',class_ = "article_teaser_body")
+    new_list_title = []
+
+    #Run for loop that outputs the news paragraph
+    for answer in results_2:
+        new_list_title.append(answer.text)
+    news_p = new_list_title[0]
+    news_p
+
+def featured_image_scrape():
